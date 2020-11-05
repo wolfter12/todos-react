109e9f5feec56f958fa9b2bf43a9ea2354ed52a7
Nazar <wolfter12@gmail.com>
2020-11-05 23:52:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import TodoList from './Todo/TodoList';
+import Context from './context';
+import Loader from './Loader';
+import AddTodo from './Todo/AddTodo';
 
 function App() {
+  const [TODOS, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000)
+      });
+  }, []);
+
+  function addTodo(title) {
+    setTodos(
+      TODOS.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        }
+      ])
+    );
+  }
+
+  const toggleTodo = (id) => {
+    setTodos(
+      TODOS.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+
+  function removeTodo(id) {
+    setTodos(TODOS.filter(todo => todo.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className='wrapper'>
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo} />
+
+        {loading && <Loader />}
+        {
+          TODOS.length
+            ? <TodoList todos={TODOS} onToggle={toggleTodo} />
+            : loading
+              ? null
+              : <p>No todos</p>
+        }
+
+      </div>
+    </Context.Provider>
   );
 }
 
